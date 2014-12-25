wolfv/josm

commit 7ce8b1e5ecfd937d31e68730f065958238ee6350

fix/debug problems seen on Jenkins: - delete old windows installer files before building new one - better error message for unexplained regression + attempt to fix it by synchronization in CachedFile
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7853 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/CachedFile.java
@@ -65,7 +65,7 @@ public class CachedFile {
     protected CachingStrategy cachingStrategy;
 
     protected File cacheFile = null;
-    boolean initialized = false;
+    protected boolean initialized = false;
 
     public static final long DEFAULT_MAXTIME = -1L;
     public static final long DAYS = 24*60*60; // factor to get caching time in days
@@ -177,7 +177,9 @@ public class CachedFile {
                 if (is == null)
                     throw new IOException(tr("Failed to open input stream for resource ''{0}''", name));
                 return is;
-            } else throw new IOException();
+            } else {
+                throw new IllegalStateException("No file found for something different from a resource: "+name);
+            }
         }
         return new FileInputStream(file);
     }
@@ -188,7 +190,7 @@ public class CachedFile {
      * returns just that file.
      * @throws IOException when the resource with the given name could not be retrieved
      */
-    public File getFile() throws IOException {
+    public synchronized File getFile() throws IOException {
         if (initialized)
             return cacheFile;
         initialized = true;

windows/josm-setup-unix.sh
@@ -120,7 +120,7 @@ function build_exe {
 	# (an exe file makes attaching to file extensions a lot easier)
 	# launch4j - http://launch4j.sourceforge.net/
 	# delete old exe file first
-	/bin/rm -f ${TARGET}.exe
+	/bin/rm -f ${TARGET}*.exe
 	$LAUNCH4J "launch4j_${TARGET}.xml"
 	# comment previous line and uncomment next one on Windows
 	#"$LAUNCH4J" "launch4j_${TARGET}.xml"

commit 0054e00e2a051bb2e0e08609ec715bbef948dd7d

fix #10884 - crash at startup (regression from r7853)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7890 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/io/CachedFile.java
@@ -178,7 +178,7 @@ public class CachedFile {
                     throw new IOException(tr("Failed to open input stream for resource ''{0}''", name));
                 return is;
             } else {
-                throw new IllegalStateException("No file found for something different from a resource: "+name);
+                throw new IOException("No file found for: "+name);
             }
         }
         return new FileInputStream(file);