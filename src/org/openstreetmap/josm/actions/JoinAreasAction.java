wolfv/josm

commit 894aa8c7ed4994983bbc42051bac79e59b7f526d

update presets lang, update Groovy, remove deprecated stuff, fix some minor issues
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7987 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -477,7 +477,7 @@ public class JoinAreasAction extends JosmAction {
                     allWays.add(pol.outerWay);
                     allWays.addAll(pol.innerWays);
                 }
-                DataSet ds = Main.main.getCurrentDataSet();
+                DataSet ds = ways.iterator().next().getDataSet();
                 if (ds != null) {
                     ds.setSelected(allWays);
                     Main.map.mapView.repaint();

build.xml
@@ -278,7 +278,7 @@ Build-Date: ${build.tstamp}
             <include name="**/*.jar"/>
         </fileset>
         <pathelement path="dist/josm-custom.jar"/>
-        <pathelement path="tools/groovy-all-2.3.9.jar"/>
+        <pathelement path="tools/groovy-all-2.4.0.jar"/>
     </path>
     <macrodef name="init-test-preferences">
         <attribute name="testfamily"/>
@@ -325,7 +325,7 @@ Build-Date: ${build.tstamp}
         </sequential>
     </macrodef>
     <target name="test-compile" depends="test-init,dist">
-        <taskdef name="groovyc" classname="org.codehaus.groovy.ant.Groovyc" classpath="tools/groovy-all-2.3.9.jar"/>
+        <taskdef name="groovyc" classname="org.codehaus.groovy.ant.Groovyc" classpath="tools/groovy-all-2.4.0.jar"/>
         <call-groovyc testfamily="unit">
             <cp-elements>
                 <path refid="test.classpath"/>

src/org/openstreetmap/josm/actions/PasteAction.java
@@ -92,7 +92,7 @@ public final class PasteAction extends JosmAction implements PasteBufferChangedL
         // We previously checked for modifier to know if the action has been trigerred via shortcut or via menu
         // But this does not work if the shortcut is changed to a single key (see #9055)
         // Observed behaviour: getActionCommand() returns Action.NAME when triggered via menu, but shortcut text when triggered with it
-        if (!getValue(NAME).equals(e.getActionCommand())) {
+        if (e != null && !getValue(NAME).equals(e.getActionCommand())) {
             final Point mp = MouseInfo.getPointerInfo().getLocation();
             final Point tl = Main.map.mapView.getLocationOnScreen();
             final Point pos = new Point(mp.x-tl.x, mp.y-tl.y);

src/org/openstreetmap/josm/data/Preferences.java
@@ -1683,13 +1683,6 @@ public class Preferences {
         }
 
         String[] obsolete = {
-                "validator.tests",                             // 01/2014 - can be removed end-2014. Replaced by validator.skip
-                "validator.testsBeforeUpload",                 // 01/2014 - can be removed end-2014. Replaced by validator.skipBeforeUpload
-                "validator.TagChecker.sources",                // 01/2014 - can be removed end-2014. Replaced by validator.TagChecker.source
-                "validator.TagChecker.usedatafile",            // 01/2014 - can be removed end-2014. Replaced by validator.TagChecker.source
-                "validator.TagChecker.useignorefile",          // 01/2014 - can be removed end-2014. Replaced by validator.TagChecker.source
-                "validator.TagChecker.usespellfile",           // 01/2014 - can be removed end-2014. Replaced by validator.TagChecker.source
-                "validator.org.openstreetmap.josm.data.validation.tests.MapCSSTagChecker.sources" // 01/2014 - can be removed end-2014. Replaced by validator.org.openstreetmap.josm.data.validation.tests.MapCSSTagChecker.entries
         };
         for (String key : obsolete) {
             if (settingsMap.containsKey(key)) {

commit 17ac75cdf7f374d9f3ccbefd06eeb56d366e5c26

fix #11026 - NPE when joining areas (regression from r7987)
git-svn-id: https://josm.openstreetmap.de/svn/trunk@7989 0c6e7542-c601-0410-84e7-c038aed88b3b

src/org/openstreetmap/josm/actions/JoinAreasAction.java
@@ -462,6 +462,12 @@ public class JoinAreasAction extends JosmAction {
         //user canceled, do nothing.
 
         try {
+            // see #11026 - Because <ways> is a dynamic filtered (on ways) of a filtered (on selected objects) collection,
+            // retrieve effective dataset before joining the ways (which affects the selection, thus, the <ways> collection)
+            // Dataset retrieving allows to call this code without relying on Main.getCurrentDataSet(), thus, on a mapview instance
+            DataSet ds = ways.iterator().next().getDataSet();
+
+            // Do the job of joining areas
             JoinAreasResult result = joinAreas(areas);
 
             if (result.hasChanges) {
@@ -477,7 +483,6 @@ public class JoinAreasAction extends JosmAction {
                     allWays.add(pol.outerWay);
                     allWays.addAll(pol.innerWays);
                 }
-                DataSet ds = ways.iterator().next().getDataSet();
                 if (ds != null) {
                     ds.setSelected(allWays);
                     Main.map.mapView.repaint();